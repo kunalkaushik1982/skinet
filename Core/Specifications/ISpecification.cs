2f8bb3d313bf55c43eeace8d2d7fc111ed40000f
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2021-08-14 19:41:18 +0530

    End of section 6

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -8,6 +8,16 @@ namespace Core.Specifications
     {
         Expression<Func<T,bool>> Criteria{get;}
         List<Expression<Func<T,Object>>> Includes{get;}
+
+        Expression<Func<T,object>> OrderBy {get;}
+
+        Expression<Func<T,object>> OrderByDescending {get;}
+
+        int Take{get;}
+
+        int Skip{get;}
+
+        bool IsPagingEnabled{get;}
         
     }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Core.Entities;
@@ -16,10 +17,23 @@ namespace Infrastructure.Data
         public DbSet<ProductBrand> ProductBrands{get;set;}
         public DbSet<ProductType> ProductTypes { get; set; }
 
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            if(Database.ProviderName=="Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p=>p.PropertyType == typeof(decimal));
+                    
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+                }
+            }
         }
 
         public Task<object> GetProductBrandsAsync()
